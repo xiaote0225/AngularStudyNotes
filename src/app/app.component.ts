1d0346e9c595828d468b4ebdcfd574d3e9190d57
wangxun <wangxun@cyai.com>
2022-01-09 09:35:03 +0800

    feat:显示数据;

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,24 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  // templateUrl: './app.component.html',
+  template: `
+    <h1 class="title">{{title}}</h1>
+    <p>name666:{{heroName}}</p>
+    <div>{{testVal}}</div>
+  `,
+  // styleUrls: ['./app.component.scss']
+  styles: [`
+    .title,p{
+      color: red;
+    }
+    div{
+      color: pink;
+    }
+  `]
 })
 export class AppComponent {
+  title = 'hero';
+  heroName = '王逊';
+  testVal = '这是一个测试对应的值';
 }
