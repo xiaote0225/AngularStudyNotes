0d4afae3debaa1a2ad8b75a26f37dbbee9ca50d8
wangxun <wangxun@cyai.com>
2022-01-09 09:52:49 +0800

    feat:模板表达式;

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,24 +2,36 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  // templateUrl: './app.component.html',
-  template: `
-    <h1 class="title">{{title}}</h1>
-    <p>name666:{{heroName}}</p>
-    <div>{{testVal}}</div>
-  `,
-  // styleUrls: ['./app.component.scss']
-  styles: [`
-    .title,p{
-      color: red;
-    }
-    div{
-      color: pink;
-    }
-  `]
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+  // template: `
+  //   <h1 class="title">{{title}}</h1>
+  //   <p>name666:{{heroName}}</p>
+  //   <div>{{testVal}}</div>
+  // `,
+  // styles: [`
+  //   .title,p{
+  //     color: red;
+  //   }
+  //   div{
+  //     color: pink;
+  //   }
+  // `]
 })
 export class AppComponent {
   title = 'hero';
   heroName = '王逊';
   testVal = '这是一个测试对应的值';
+
+  price = 100;
+
+  gender = 1;
+
+  get job(): string{
+    return '战士';
+  }
+
+  getVal(): number{
+    return 666;
+  }
 }
