4943bf9d61d052de35c777f763c31bacf0a36592
wangxun <wangxun@cyai.com>
2022-01-09 11:58:15 +0800

    feat:创建组件;

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Com1Component } from './com1/com1.component';
+import { Com2Component } from './com2/com2.component';
+import { Com3Component } from './com3/com3.component';
+import { Com4Component } from './com4/com4.component';
+import { Com5Component } from './com5/com5.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Com1Component,
+    Com2Component,
+    Com3Component,
+    Com4Component,
+    Com5Component
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  exports: [
+    Com4Component,
+    Com5Component
+  ]
 })
 export class AppModule { }
