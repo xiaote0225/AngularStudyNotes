1af6777328099745533ce8a46f476e7092b383f0
wangxun <wangxun@cyai.com>
2022-01-09 09:21:21 +0800

    feat:删除无用的module模块;

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'my-module\my-module', loadChildren: () => import('./my-module/my-module/my-module.module').then(m => m.MyModuleModule) }, { path: 'test1', loadChildren: () => import('./my-module/test1/test1.module').then(m => m.Test1Module) }];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { M1Module } from './my-module/m1/m1/m1.module';
-import { Test2Module } from './my-module/test2/test2.module';
 
 @NgModule({
   declarations: [
@@ -13,8 +11,6 @@ import { Test2Module } from './my-module/test2/test2.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    M1Module,
-    Test2Module
   ],
   providers: [],
   bootstrap: [AppComponent]
