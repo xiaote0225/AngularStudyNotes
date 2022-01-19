7ce71e6e523bc63a10d7aa9a84fedc4afd3c226c
wangxun <wangxun@cyai.com>
2022-01-19 18:32:46 +0800

    feat:获取template

--- src/app/demos/components/content-child/content-child.component.ts
+++ src/app/demos/components/content-child/content-child.component.ts
@@ -1,6 +1,5 @@
-import { Component, ContentChild, OnInit, ElementRef, ContentChildren, QueryList } from '@angular/core';
+import { Component, ContentChild, OnInit, ElementRef, ContentChildren, QueryList, TemplateRef } from '@angular/core';
 import { ChidComponentComponent } from './chid-component/chid-component.component';
-import { PanelComponent } from './panel/panel.component';
 
 @Component({
   selector: 'app-content-child',
@@ -16,7 +15,8 @@ export class ContentChildComponent implements OnInit {
   @ContentChild(ChidComponentComponent,{static:true})private chidComponentInstatnce2:ChidComponentComponent;
   @ContentChildren(ChidComponentComponent,{descendants:true})private chidComponentInstatnce2List:QueryList<ChidComponentComponent>;
   @ContentChild('lalala',{static:true,read:ChidComponentComponent})private lalalaInstatnce:ChidComponentComponent;
-
+  @ContentChild('template',{static:true})private templateInstance:TemplateRef<any>;
+  @ContentChild(TemplateRef)private templateInstance1:TemplateRef<any>;
   constructor() {
     console.log('constructor---this.listEl1---',this.listEl1);
     console.log('constructor---this.listEl---',this.listEl);
@@ -26,6 +26,8 @@ export class ContentChildComponent implements OnInit {
     console.log('constructor---this.chidComponentInstatnce2---',this.chidComponentInstatnce2);
     console.log('constructor---this.chidComponentInstatnce2List---',this.chidComponentInstatnce2List);
     console.log('constructor---this.lalalaInstatnce---',this.lalalaInstatnce?.age);
+    console.log('constructor---this.templateInstance---',this.templateInstance);
+    console.log('constructor---this.templateInstance1---',this.templateInstance1);
   }
 
 
@@ -38,6 +40,8 @@ export class ContentChildComponent implements OnInit {
     console.log('ngOnInit---this.chidComponentInstatnce2---',this.chidComponentInstatnce2);
     console.log('ngOnInit---this.chidComponentInstatnce2List---',this.chidComponentInstatnce2List);
     console.log('ngOnInit---this.lalalaInstatnce---',this.lalalaInstatnce?.age);
+    console.log('ngOnInit---this.templateInstance---',this.templateInstance);
+    console.log('ngOnInit---this.templateInstance1---',this.templateInstance1);
   }
 
   ngAfterViewInit(): void {
@@ -49,6 +53,8 @@ export class ContentChildComponent implements OnInit {
     console.log('ngAfterViewInit---this.chidComponentInstatnce2---',this.chidComponentInstatnce2);
     console.log('ngAfterViewInit---this.chidComponentInstatnce2List---',this.chidComponentInstatnce2List);
     console.log('ngAfterViewInit---this.lalalaInstatnce---',this.lalalaInstatnce?.age);
+    console.log('ngAfterViewInit---this.templateInstance---',this.templateInstance);
+    console.log('ngAfterViewInit---this.templateInstance1---',this.templateInstance1);
   }
 
 }
