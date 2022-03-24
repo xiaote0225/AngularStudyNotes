6f82110831658a89adf0ca8207aedd38519002a0
wangxun <wangxun@cyai.com>
2022-03-24 11:16:25 +0800

    feat:7.2响应式表单验证

--- src/app/form-study/profile-editor/profile-editor.component.ts
+++ src/app/form-study/profile-editor/profile-editor.component.ts
@@ -1,5 +1,19 @@
-import { FormArray, FormBuilder } from '@angular/forms';
+import { HasMobileService } from './../has-mobile.service';
+import { FormArray, FormBuilder, ValidatorFn, AbstractControl, ValidationErrors, FormGroup, Validators, AsyncValidator } from '@angular/forms';
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+
+function forbiddenNameValidator(req:RegExp):ValidatorFn{
+  return (control:AbstractControl):ValidationErrors | null => {
+    const forbidden = req.test(control.value);
+    return forbidden ? {forbiddenName:{value:'名字不能包含bob'}}: null;
+  }
+}
+
+function equalValidator(group:FormGroup):ValidationErrors | null {
+  const password = group.get('password');
+  const rePassword = group.get('rePassword');
+  return password?.value === rePassword?.value ? null : {equal: '两次密码不一样'};
+}
 
 @Component({
   selector: 'app-profile-editor',
@@ -10,55 +24,79 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 export class ProfileEditorComponent implements OnInit {
 
   profileForm = this.fb.group({
-    firstName: [''],
-    lastName: [''],
-    address: this.fb.group({
-      street: [''],
-      city: [''],
-      state: [''],
-      zip: ['']
-    }),
-    aliases: this.fb.array([
-      this.fb.control('')
-    ])
+    firstName:['',Validators.required],
+    lastName:['',[
+      Validators.required,
+      Validators.minLength(4),
+      forbiddenNameValidator(/bob/i)
+    ]],
+    mobile:['',{
+      validators:[
+        Validators.required,
+        Validators.pattern(/^\d{11}$/)
+      ],
+      asyncValidators:this.hasMobileServe.validate,
+      updateOn:'blur'
+    }],
+    newPass:this.fb.group({
+      password:['',Validators.required],
+      rePassword:['',Validators.required]
+    },{validators:equalValidator})
   });
 
-  get aliases(){
-    return this.profileForm.get('aliases') as FormArray;
+  get firstName(){
+    return this.profileForm.get('firstName');
   }
 
-  addAlias(){
-    this.aliases.push(this.fb.control(''));
+  get lastName(){
+    return this.profileForm.get('lastName');
   }
+
+  get mobile(){
+    return this.profileForm.get('mobile');
+  }
+
+  get newPass(){
+    return this.profileForm.get('newPass')
+  }
+
+  // get aliases(){
+  //   return this.profileForm.get('aliases') as FormArray;
+  // }
+
+
+  // addAlias(){
+  //   this.aliases.push(this.fb.control(''));
+  // }
 
   onSubmit(){
     console.log('onSubmit',this.profileForm.value);
     console.log('lastName',this.profileForm.get('lastName')?.value);
   }
 
-  updateProfile(){
-    this.profileForm.patchValue({
-      firstName: 'Nancy',
-      address:{
-        street: '123 Drew Street'
-      }
-    });
-  }
+  // updateProfile(){
+  //   this.profileForm.patchValue({
+  //     firstName: 'Nancy',
+  //     address:{
+  //       street: '123 Drew Street'
+  //     }
+  //   });
+  // }
 
-  updateProfile2(){
-    this.profileForm.setValue({
-      firstName:'Nancy45646',
-      lastName:'',
-      address:{
-        street:'123 Drew Street',
-        city:'',
-        state:'',
-        zip:''
-      }
-    });
-  }
+  // updateProfile2(){
+  //   this.profileForm.setValue({
+  //     firstName:'Nancy45646',
+  //     lastName:'',
+  //     address:{
+  //       street:'123 Drew Street',
+  //       city:'',
+  //       state:'',
+  //       zip:''
+  //     }
+  //   });
+  // }
 
-  constructor(private fb:FormBuilder) { }
+  constructor(private fb:FormBuilder,private hasMobileServe:HasMobileService) { }
 
   ngOnInit(): void {
   }
