9ba384d4a8681c3680e8f5a9c7f0e668788dadf3
wangxun <wangxun@cyai.com>
2022-03-29 11:58:00 +0800

    feat:8.21-按钮权限指令

--- src/app/pages/home/heroes/heroes.module.ts
+++ src/app/pages/home/heroes/heroes.module.ts
@@ -4,11 +4,12 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HeroesComponent } from './heroes.component';
 import { SpinComponent } from './spin/spin.component';
+import { AuthDirective } from './auth.directive';
 
 
 
 @NgModule({
-  declarations: [HeroesComponent, SpinComponent],
+  declarations: [HeroesComponent, SpinComponent, AuthDirective],
   imports: [
     CommonModule,
     FormsModule,
