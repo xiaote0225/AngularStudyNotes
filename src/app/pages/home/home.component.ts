36683d664396a7605a63ac92f565d7e9df4aebaf
wangxun <wangxun@cyai.com>
2022-03-29 18:44:45 +0800

    feat:9.5-路由动画

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,8 +1,9 @@
+import { animate, animateChild, group, query, style, transition, trigger } from '@angular/animations';
 import { AuthKey } from './../../configs/constant';
 import { AccountService } from './../../services/account.service';
 import { UserService } from './../../services/user.service';
 import { filter, switchMap } from 'rxjs/operators';
-import { ActivatedRoute, Router, NavigationEnd } from '@angular/router';
+import { ActivatedRoute, Router, NavigationEnd, RouterOutlet } from '@angular/router';
 import { Component, OnInit, ChangeDetectionStrategy, ChangeDetectorRef, Inject } from '@angular/core';
 import { Hero } from 'src/app/types';
 import { combineLatest } from 'rxjs';
@@ -13,7 +14,34 @@ import { WindowService } from 'src/app/services/window.service';
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  animations:[
+    trigger('routeAnimations',[
+      transition('* <=> *',[
+        style({position:'relative'}),
+        query(':enter,:leave',[
+          style({
+            position:'absolute',
+            top:0,
+            left:0,
+            width:'100%'
+          })
+        ],{optional:true}),
+        query(':enter',[
+          style({left:'-100%'})
+        ],{optional:true}),
+        group([
+          query(':leave',[
+            animate(300,style({left:'100%'}))
+          ],{optional:true}),
+          query(':enter',[
+            animate(300,style({left:0}))
+          ],{optional:true})
+        ]),
+        query(':enter',animateChild())
+      ])
+    ])
+  ]
 })
 export class HomeComponent implements OnInit {
   currentUser:Hero | null;
@@ -68,6 +96,10 @@ export class HomeComponent implements OnInit {
     //   }, 500);
     // });
     // this.currentUser$ = this.userServe.getUser();
+  }
+
+  prepareRoute(outlet:RouterOutlet){
+    return outlet && outlet.activatedRouteData && outlet.activatedRouteData['animation'];
   }
 
 }
--- src/app/pages/home/user-center/user-center.component.ts
+++ src/app/pages/home/user-center/user-center.component.ts
@@ -1,13 +1,26 @@
+import { stagger, animate, style, trigger, transition, query } from '@angular/animations';
 import { UserService } from './../../../services/user.service';
 import { Observable } from 'rxjs';
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, HostBinding, OnInit } from '@angular/core';
 import { Hero } from 'src/app/types';
 
 @Component({
   selector: 'app-user-center',
   templateUrl: './user-center.component.html',
   styleUrls: ['./user-center.component.scss'],
-  changeDetection:ChangeDetectionStrategy.OnPush
+  changeDetection:ChangeDetectionStrategy.OnPush,
+  animations:[
+    trigger('pageAnimations',[
+      transition(':enter',[
+        query('.user-center',[
+          style({opacity:0,transform:'translateY(-100px)'}),
+          stagger(300,[
+            animate('500ms cubic-bezier(0.35,0,0.25,1)',style({opacity:1,transform:'none'}))
+          ])
+        ])
+      ])
+    ])
+  ]
 })
 export class UserCenterComponent implements OnInit {
   user$:Observable<Hero | null>;
@@ -16,6 +29,8 @@ export class UserCenterComponent implements OnInit {
     this.user$ = this.userServe.user$;
   }
 
+  @HostBinding('@pageAnimations')
+  public animatePage = true;
   ngOnInit(): void {
   }
 
